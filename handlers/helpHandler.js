b863a054c29439db2b79f7e8ebe663b64580daa2
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2024-07-15 00:23:38 +0530

    Update helpHandler.js

--- handlers/helpHandler.js
+++ handlers/helpHandler.js
@@ -1,10 +1,39 @@
-// handlers/helpHandler.js
 import { sendWhatsAppMessage } from '../lib/sendWhatsappMessage';
 
 export const handleHelp = async (userPhoneNumber) => {
     try {
         const templateName = 'help';
-        await sendWhatsAppMessage(userPhoneNumber, templateName, ' ');
+        const bodyValues = [
+            'VD MH14TC4540 - Check vehicle registration details',
+            'Challan MH14TC4540 - Search for traffic violations',
+            'Fasttag MH14TC4540 - Find any vehicle\'s FASTag information',
+            'CVN MA1NMC57111 - Chassis to Vehicle Number (coming soon)',
+            'OSINT 98XXXXXXX5 - Advanced people search',
+            'EMAIL test@gmail.com - Easily search for Gmail-related information',
+            'SMS VM-HDFCBN - Decode SMS headers, company info, and more.',
+            'Name 98XXXXXX80 - Identify callers',
+            'VN +44042009560 - Uncover virtual number details',
+            'PAN BEL81E - Retrieve PAN card information',
+            'GST 27AADCT73G1Z3 - Retrieve GST information',
+            'PGST BEL95C - Retrieve PAN TO GST Information',
+            'IPL https://google.com - Track IP/GPS (copy & share the generated link)',
+            'IPSTOP #XDTRFGTZ - Delete an IP Log link',
+            'IP 192.168.0.01 - Lookup IP address details',
+            'PIN 411044 - Get Pin Code information',
+            'IMEI 35920003041526 - Find device model details',
+            'FULL IMEI 35920003041526 - Find the last digit of an IMEI',
+            'PNR 845257456 - Check train ticket status & info',
+            'IFSC KKBK0062517 or IFSC KOTAH CHINC - Find bank & IFSC codes',
+            'UPI 9XXXXXX567 - Get UPI ID details',
+            'CC Vikas - Track court cases',
+            'GAS 9XXXXX2345 - Link phone numbers to gas connections',
+            'BTS 404-434903227 - Cell tower info on Google Maps',
+            'Network 9XXXXX2345 - Mobile number portability details',
+            'Validity - Check your subscription status',
+            'Support - Get assistance'
+        ];
+
+        await sendWhatsAppMessage(userPhoneNumber, templateName, '', bodyValues);
     } catch (error) {
         console.error('Error sending help message:', error);
         throw new Error('Failed to send help message');
